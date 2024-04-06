5c79bca46dce7c4d76b14fa135ada5805095c54c
Romario <roman20011102@gmail.com>
2024-04-06 16:17:44 +0400

    Update task3.py

--- week2/task3.py
+++ week2/task3.py
@@ -1,6 +1,6 @@
 import json
 from random import shuffle
-from itertools import permutations
+from itertools import permutations, combinations_with_replacement
 
 def writef(filename, data):
     with open(filename, 'w') as f:
@@ -50,7 +50,7 @@ def decrypt(k):
     keys = list(permutations(base))
     for key in keys:
         key = list(key) + [key[0]]
-        f.write('\nКлюч ' + str(key) + ":\n    ")
+        f.write('\n' + str(key[:-1 ]) + ":\n    ")
         for i in range(len(plaintext) // k + 1):
             enc_block = plaintext[i * k:(i + 1) * k]
             block = [""] * k
@@ -60,9 +60,26 @@ def decrypt(k):
 
     f.close()
 
+def get_alphabet():
+    with open("open_text.txt", 'r', encoding='utf-8') as file:
+        text = file.read().upper()
+        alphabet = ''.join(set(text))
 
-def find_all_bigrams():
-    with open("open_text.txt", 'r') as file:
+    writef("alphabet.txt", {'alphabet': alphabet})
+    return alphabet
+
+
+def find_forbidden_bigrams():
+    alphabet = readf('alphabet.txt')['alphabet']
+    all_bigrams = set()
+    for a, b in combinations_with_replacement(alphabet, 2):
+        all_bigrams.add(a + b)
+    forbidden_bigrams = all_bigrams - find_all_bigrams_in_text()
+    writef('forbidden_bigrams.txt', {'bigrams' : list(forbidden_bigrams)})
+
+
+def find_all_bigrams_in_text():
+    with open("open_text.txt", 'r', encoding='utf-8') as file:
         text = []
         for line in file:
             text.append(list(line.upper()))
@@ -72,48 +89,47 @@ def find_all_bigrams():
         for j in range(1, len(text[i])-1):
             bigrams.add(text[i][j-1] + text[i][j])
 
-    writef("forbidden_bigrams.txt", {"bigrams": list(bigrams)})
     return bigrams
-
-
-def get_alphabet():
-    with open("open_text.txt", 'r') as file:
-        text = file.read().upper()
-        alphabet = ''.join(set(text))
-
-    writef("alphabet.txt", {'alphabet': alphabet})
-    return alphabet
 
 
 def get_sup_table(k):
     bigrams = readf("forbidden_bigrams.txt")['bigrams']
     table = [['0'] * k for _ in range(k)]
     with open("enc_text.txt", 'r') as enc_file:
-        enc = enc_file.read()[:k * k]
+        enc = enc_file.read().upper()
 
     enc = list(enc)
     enc_k = []
-    for i in range(k):
-        enc_k.append(enc[i:i + k])
+    for j in range(len(enc) // k):
+        enc_k.append(enc[j:j + k])
 
-    # for row in enc_k:
-    #     print(row)
+    for row in range(k):
+        print(enc_k[row])
     # print(bigrams)
-
-    for i in range(k - 1):
+    for i in range(k):
         for j in range(k):
-            if enc_k[j][i] + enc_k[j][i + 1] in bigrams:
-                print(enc_k[j][i] + enc_k[j][i + 1])
-                table[i][j] = 'X'
+            for l in range(k):
+                if i == j or (enc_k[l][i] + enc_k[l][j] in bigrams):
+                    # print(i + 1, j + 1, l + 1, enc_k[l][i] + enc_k[l][j])
+                    table[i][j] = 'X'
+                    # for row in table:
+                    #     print(''.join(row))
 
+    print('Вспомогательная таблица:')
     with open("sup_table.txt", 'w') as f:
         for row in table:
+            print(''.join(row))
             f.write(''.join(row) + '\n')
 
 
-# find_all_bigrams()
+def get_keys_tree():
+    pass
+
+
+k = 5
 # get_alphabet()
-k = 6
+# find_forbidden_bigrams()
 # gen_key(k)
 # encrypt()
 get_sup_table(k)
+decrypt(k)
